961d15e7e944a53c2ee0b231bb648031b51fa47c
Sanjay Kumar <sanjay.kumar@apogeephysicians.com>
2017-10-18 15:34:09 -0500

    handle manual

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ErrorHandler, Injector} from '@angular/core';
+import * as Rollbar from 'rollbar';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private injector: Injector) { }
   title = 'Angular 4 with Rollbar';
   errorMessage = 'Hello World, Error Message';
 
@@ -13,5 +16,9 @@ export class AppComponent {
     if ( 1 === 1 ) {
       throw new Error(this.errorMessage);
     }
+  }
+  manualHandle() {
+    const rollbar = this.injector.get(Rollbar);
+      rollbar.error(this.errorMessage);
   }
 }
